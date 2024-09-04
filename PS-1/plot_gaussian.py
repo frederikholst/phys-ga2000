7c70d22b12a2a4e4c54059485b8cd7d789e2df09
frederikholst <frederikholstknudsen@gmail.com>
2024-09-05 01:11:42 +0200

    second push

--- PS-1/plot_gaussian.py
+++ PS-1/plot_gaussian.py
@@ -5,13 +5,17 @@ def Gaussian(x):
     Input: float
     Ouput: Value of Gaussian at that x-value
     """
-    sig=2
+    sig=3
     
-    return 1/(sig*np.sqrt(2*np.pi))*np.exp(-(x+3)**2/(2*sig**2))
+    return 1/(sig*np.sqrt(2*np.pi))*np.exp(-(x)**2/(2*sig**2))
 y_values=np.array([])
-x_values=np.arange(-10,10)
+x_values=np.arange(-10,11)
+
 for i in x_values:
     y_values=np.append(y_values,Gaussian(i))
 
 plt.plot(x_values,y_values)
+plt.title("Gaussian distribution")
+plt.ylabel("y")
+plt.xlabel("x")
 plt.savefig("gaussian.png")
