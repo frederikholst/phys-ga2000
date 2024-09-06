adb91c7f63f0df21759cb9bb3fb75e1cb1b27354
frederikholst <frederikholstknudsen@gmail.com>
2024-09-06 18:16:59 +0200

    3. push

--- PS-1/plot_gaussian.py
+++ PS-1/plot_gaussian.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 def Gaussian(x):
     """
     Input: float
@@ -7,15 +8,15 @@ def Gaussian(x):
     """
     sig=3
     
-    return 1/(sig*np.sqrt(2*np.pi))*np.exp(-(x)**2/(2*sig**2))
+    return 1/(sig*np.sqrt(2*np.pi))*np.exp(-(x**2)/(2*sig**2))
 y_values=np.array([])
-x_values=np.arange(-10,11)
-
+x_values=np.arange(-10,11,0.1)
 for i in x_values:
     y_values=np.append(y_values,Gaussian(i))
 
 plt.plot(x_values,y_values)
-plt.title("Gaussian distribution")
+
+plt.title("Gaussian Distribution")
 plt.ylabel("y")
 plt.xlabel("x")
 plt.savefig("gaussian.png")
